e44fe83aead75f99709828b2866653d5b8945b5e
kwraight <kenneth.gibb.wraight@gmail.com>
2019-10-04 11:32:58 +0100

    for home project

---
+++ connect_homeTemp.py
@@ -0,0 +1,37 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+try:
+    import usocket as socket
+except:
+    import socket
+
+from machine import Pin
+import network
+import myDetails
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+myWifi = myDetails.SetHomekWifi()
+ssid = myWifi.ssid
+password = myWifi.pwd
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+    pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+led = Pin(2, Pin.OUT)
+
+'''
+some pins connected to sensors
+'''
---
+++ server_homeTemp.py
@@ -0,0 +1,45 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+def web_page(dicts):
+
+    html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+                h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none;
+                border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+                .button2{background-color: #4286f4;}</style></head><body> <h1>Temp Sensing...</h1>
+                <p><strong>""" + name + """</strong> is the best 123! </p>
+                    </body></html>"""
+    html+="""<p>"""
+    for d in dicts:
+        html+="""{"""+d['name']+""":"""+str(d['val'])+"""}"""
+    html+="""</p>"""
+    return html
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+dicts=[]
+dicts.append({'name':"temperature",'nix':'temp','x':0.0})
+dicts.append({'name':"humidity",'nix':'humi','x':0.0})
+
+while True:
+    conn, addr = s.accept()
+    print('Got a connection from %s' % str(addr))
+    request = conn.recv(1024)
+    request = str(request)
+    print('Content = %s' % request)
+
+    '''
+    read pins, set values
+    '''
+
+    response = web_page(dicts)
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    try:
+        conn.sendall(response)
+    except OSError:
+        print("not right now!")
+    conn.close()
